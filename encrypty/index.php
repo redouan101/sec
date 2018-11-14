42a78a6f4bac774a92d11ce1b8eb739eddac5efe
redouan101 <34091823+redouan101@users.noreply.github.com>
2018-11-14 12:27:44 +0100

    Update index.php

--- encrypty/index.php
+++ encrypty/index.php
@@ -1,4 +1,32 @@
 <?php
+    $word = "hallo";
+    $length = strlen($word);
+$result = [];
+for ($i = 0; $i < $length; $i++){
+    if ($i % 2 == 0){
+        $cijfer = ord($word[$i]);
+        $wordencrypt = $cijfer + $i;
+        echo chr($wordencrypt);
+    } else {
+        $cijfer = ord($word[$i]);
+        $wordencrypt = $cijfer - $i;
+        echo chr($wordencrypt);
+    }
+}
+echo '<br><br>';
+echo 'opdr 2 <br>';
+echo 'AES, Blowfish, Camellia, SEED, CAST-128, DES, IDEA, RC2, RC4, RC5, Triple DES, GOST 28147-89';
 
-$string = 'hallo';
-for ($i) a niffauw         
+echo '<br><br>';
+echo 'opdr 3<br>';
+echo 'openssl_encrypt — Encrypts data';
+echo 'openssl_pkcs7_encrypt — Encrypt an S/MIME message';
+echo 'openssl_private_encrypt — Encrypts data with private key';
+echo 'openssl_public_encrypt — Encrypts data with public key';
+echo '<br>';
+echo 'openssl_decrypt — Decrypts data';
+echo 'openssl_pkcs7_decrypt — Decrypts an S/MIME encrypted message';
+echo 'openssl_private_decrypt — Decrypts data with private key';
+echo 'openssl_public_decrypt — Decrypts data with public key';
+echo '<br><br>';
+     
